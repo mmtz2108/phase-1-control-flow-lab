2ee535a713b52a3e7c3169a0b4cf8def74a57ab3
Marcos Martinez <mmtz2108@gmail.com>
2022-04-04 21:26:32 -0500

    commit tree 4c903053cc8214be30d174ac573b1d3a46c6e352

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(trip) {
+  if (trip <= 400) {
+    return `This one is on me!`
+  } else if (trip > 400 && trip < 2000) {
+    return `That will be twenty bucks.`
+  } else if (trip > 2000 && trip <= 2500) {
+    return `I will gladly take your thirty bucks.`
+  } else if (trip > 2500) {
+    return `No can do.`
+  };
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destination) {
+  return destination === 'NYC' ? 'Ok, sounds good.' : 'No go.'
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+    // Write your code here!
+  }};
